c7520c10a17a7cc9f85b6d3027dfa0a1c85c6f89
yangpimpollo <66030395+yangpimpollo@users.noreply.github.com>
2020-09-06 14:38:25 -0500

    NewEngine-video2

---
+++ app/src/main/java/com/example/drawbot/GamePanel.java
@@ -0,0 +1,90 @@
+package com.example.drawbot;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+    private MainThread thread;
+
+    private RectPlayer player;
+    private Point playerPoint;
+
+    public GamePanel(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+        thread = new MainThread(getHolder(), this);
+        player = new RectPlayer(new Rect(100, 100, 400, 400), Color.rgb(0, 0, 255));
+        playerPoint = new Point(150, 150);
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread = new MainThread(getHolder(), this);
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while(true){
+            try{
+                thread.setRunning(false);
+                thread.join();
+            }catch(Exception e){ e.printStackTrace(); }
+            retry = false;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                playerPoint.set((int)event.getX(), (int)event.getY());
+        }
+        return true;
+        //return super.onTouchEvent(event);
+    }
+
+    public void update(){
+        player.update(playerPoint);
+        //Toast.makeText(this, A, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void draw(Canvas canvas){
+        super.draw(canvas);
+        canvas.drawColor(Color.WHITE);
+        String A = thread.getA();
+        int ancho = canvas.getWidth();
+        int alto = canvas.getHeight();
+        Bitmap bmp= BitmapFactory.decodeResource(getResources(), R.drawable.ime);
+        canvas.drawBitmap(bmp, 0,0, null);
+        player.draw(canvas);
+        Paint pincel1 = new Paint();
+        pincel1.setARGB(255, 255, 0, 0);
+        pincel1.setTextSize(30);
+        pincel1.setTypeface(Typeface.SERIF);
+        canvas.drawText(A, 0, 300, pincel1);
+        //canvas.drawRect(200,400,100,100,pincel1);
+    }
+}
---
+++ app/src/main/java/com/example/drawbot/MainThread.java
@@ -0,0 +1,83 @@
+package com.example.drawbot;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.widget.Toast;
+
+public class MainThread extends Thread {
+    private SurfaceHolder surfaceHolder;
+    private  GamePanel gamePanel;
+    private boolean running;
+    public  static Canvas canvas;
+    private static int aps = 0;
+    private static int fps = 0;
+    private String A;
+
+    public void setRunning(boolean running){
+        this.running = running;
+    }
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+    @Override
+    public void run(){
+        final int NS_POR_SEGUNDO = 1000000000; // nanosegundos por segundo
+        final byte APS_OBJETIVO = 60; // actualizaciones por segundo
+        final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO;
+
+        long referenciaActualizacion = System.nanoTime();
+        long refernciaContador = System.nanoTime();
+        double tiempoTranscurrido;
+        double delta = 0;
+
+        while (running) {
+            canvas = null;
+            final long inicioBucle = System.nanoTime();
+            tiempoTranscurrido = inicioBucle - referenciaActualizacion;
+            referenciaActualizacion = inicioBucle;
+
+            delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;
+
+            while (delta >= 1) {
+                this.gamePanel.update();
+                aps++;
+                delta--;
+            }
+            try{
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder){
+                    this.gamePanel.draw(canvas);
+                    fps++;
+                }
+            }catch (Exception e){
+                System.out.println("Error Canvas...............");
+                e.printStackTrace();
+            }finally {
+                if(canvas != null){
+                    try{
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }catch (Exception e){ e.printStackTrace(); }
+                }
+            }
+
+            if (System.nanoTime() - refernciaContador > NS_POR_SEGUNDO) {
+                /*CONTADOR_APS = "APS : " + aps;
+                CONTADOR_FPS = "FPS : " + fps;
+                // ventana.setTitle(NOMBRE + " || APS : " + aps + " || FPS : " + fps);*/
+                //System.out.println("APS : " + aps + "  || FPS : " + fps);
+                A = "APS : " + aps + "  || FPS : " + fps;
+                //Toast.makeText(gamePanel, A, Toast.LENGTH_SHORT).show();
+                aps = 0;
+                fps = 0;
+                refernciaContador = System.nanoTime();
+            }
+        }
+
+    }
+    public String getA(){
+        return A;
+    }
+}
