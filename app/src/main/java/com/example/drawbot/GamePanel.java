57a69f83da43257170f804a7101fb9c297930b61
yangpimpollo <66030395+yangpimpollo@users.noreply.github.com>
2020-10-02 20:56:02 -0500

    screenMetrics.2020.10.02

--- app/src/main/java/com/example/drawbot/GamePanel.java
+++ app/src/main/java/com/example/drawbot/GamePanel.java
@@ -11,6 +11,7 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,6 +21,9 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     private RectPlayer player;
     private Point playerPoint;
+    private int he =MainActivity.he;
+    private int wi =MainActivity.wi;
+    private String C="wi "+wi+" | he "+he+"metrics";
 
     private final Joystick joystick;
     private PlayerB playerB;
@@ -33,7 +37,9 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         playerPoint = new Point(150, 150);
 
         joystick = new Joystick(128, 600, 70, 40);
-        playerB = new PlayerB(getContext(), 500, 500, 80);
+        //playerB = new PlayerB(getContext(), wi, he, 80);
+        playerB = new PlayerB(getContext(), wi/2, he/2, 80);
+        //System.out.println(wi+"|"+he);
         setFocusable(true);
     }
 
@@ -91,25 +97,37 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         playerB.update(joystick);
         //Toast.makeText(this, A, Toast.LENGTH_SHORT).show();
     }
+    //--------------------------------
+
+    //int ancho = canvas.getWidth();
+    //int alto = canvas.getHeight();
+    Bitmap bmp= BitmapFactory.decodeResource(getResources(), R.drawable.im);
+    Bitmap bmq= BitmapFactory.decodeResource(getResources(), R.drawable.ima);
+    //Bitmap bmpR = new BitmapRegionDecoder(new Rect(0, 0, 32, 32),  new BitmapFactory.Options());
+    Bitmap bmpR = Bitmap.createBitmap(bmp, 84, 112, 32, 32);
+    //--------------------------------
 
     @Override
     public void draw(Canvas canvas){
         super.draw(canvas);
         canvas.drawColor(Color.WHITE);
         String A = thread.getA();
-        int ancho = canvas.getWidth();
-        int alto = canvas.getHeight();
-        Bitmap bmp= BitmapFactory.decodeResource(getResources(), R.drawable.imu);
-        //Bitmap bmpR = new BitmapRegionDecoder(new Rect(0, 0, 32, 32),  new BitmapFactory.Options());
-        Bitmap bmpR = Bitmap.createBitmap(bmp, 84, 112, 32, 32);
-        canvas.drawBitmap(bmp, 0, 0, null);
+
+        int wi = canvas.getWidth();
+        int he = canvas.getHeight();
+        String B = "wi "+wi+" | he "+he+"canvas";
+
+        //canvas.drawBitmap(bmp, 0, 0, null);
+        //canvas.drawBitmap(bmq, 0, 0, null);
         //player.draw(canvas);
         Paint pincel1 = new Paint();
         pincel1.setARGB(255, 255, 0, 0);
         pincel1.setTextSize(30);
         pincel1.setTypeface(Typeface.SERIF);
         canvas.drawText(A, 50, 50, pincel1);
-        canvas.drawRect(0,168,168,336,pincel1); // *1.75
+        canvas.drawText(B, 50, 100, pincel1);
+        canvas.drawText(C, 50, 150, pincel1);
+        //canvas.drawRect(0,168,682,336,pincel1); // *1.75
 
         joystick.draw(canvas);
         playerB.draw(canvas);
--- app/src/main/java/com/example/drawbot/MainActivity.java
+++ app/src/main/java/com/example/drawbot/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.drawbot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
+    public static int wi,he;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getDimensions();
         setContentView(new GamePanel(this));
+    }
+    public void getDimensions(){
+        /*this.wi = wi;
+        this.he = he;*/
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        //int height = displayMetrics.heightPixels;
+        //int width = displayMetrics.widthPixels;
+        he = displayMetrics.heightPixels;
+        wi = displayMetrics.widthPixels;
     }
 
     //código
@@ -37,4 +49,5 @@ public class MainActivity extends Activity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
+
 }
